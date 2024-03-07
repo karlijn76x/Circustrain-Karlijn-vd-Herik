cd3d084cbc9eb6d59faccdf4164d15956c5954e6
A.M.K. van den Herik <karlijnvandenherik@gmail.com>
2024-03-07 09:01:30 +0100

    Updated circustrain

--- Circustrain Karlijn vd Herik/TrainManagement.cs
+++ Circustrain Karlijn vd Herik/TrainManagement.cs
@@ -8,6 +8,48 @@ namespace Circustrain_Karlijn_vd_Herik
 {
 	public class TrainManagement
 	{
+		private List<Wagon> wagons;
 
+		public TrainManagement() 
+		{ 
+			wagons = new List<Wagon>();
+		}
+
+		public void AddAnimalToWagon(Animal animal)
+		{
+			bool added = false;
+
+			foreach(Wagon wagon in wagons) {
+
+				if (wagon.TryAddAnimal(animal))
+				{
+					added = true;
+					break;
+				}
+			}
+
+			if (!added)
+			{
+				Wagon newWagon = new Wagon();
+				newWagon.TryAddAnimal(animal);
+				wagons.Add(newWagon);
+			}
+			DisplayTrain();
+		}
+
+		public void DisplayTrain()
+		{
+			int wagonNumber = 1;
+			foreach (var wagon in wagons)
+			{
+				Console.WriteLine($"Wagon {wagonNumber}:");
+				foreach (var animal in wagon.Animals)
+				{
+					Console.WriteLine($"- {animal.Name} ({animal.Size}), Eats Meat: {animal.EatsMeat}");
+				}
+				Console.WriteLine();
+				wagonNumber++;
+			}
+		}
 	}
 }
